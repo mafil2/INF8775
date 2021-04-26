27be580461f7b4de084aaf520f6a2a2094df8d13
mafil2 <marc-andre.filion@bell.net>
2021-04-25 20:42:20 -0400

    Better+ working version

--- TP3/main.cpp
+++ TP3/main.cpp
@@ -5,48 +5,36 @@
 
 using namespace std;
 
-struct block {
-    int netGain;
-    bool isDug;
-};
-
 struct column {
-    stack<block> blocks;
+    stack<int> blocks;
     int remainingValue;
 };
 
+vector<pair<int, int>> result;
+int bestScore = 0;
 int profit = 0;
 int n, m;
 
-//bool canDig(vector<vector<block>>& site, int i, int j) {
-//    if (site[i][j].isDug)
-//        return false;
-//
-//    return (i == 0) || 
-//           (j == 0 && site[i - 1][j].isDug && site[i - 1][j + 1].isDug) || 
-//           (j == site[i].size() - 1 && site[i - 1][j].isDug && site[i - 1][j - 1].isDug) || 
-//           (site[i - 1][j - 1].isDug && site[i - 1][j].isDug && site[i - 1][j + 1].isDug);
-//}
-
-bool canDig2(vector<column>& area, int xPos) {
+bool canDig(vector<column>& area, int xPos) {
     if (xPos < 0 || xPos > area.size() - 1)
         return false;
 
     return
         (area[xPos].blocks.size() == n) ||
-        (xPos == 0 && area[xPos + 1].blocks.size() == area[xPos].blocks.size()) ||
-        (xPos == area.size() - 1 && area[xPos - 1].blocks.size() == area[xPos].blocks.size()) ||
-        (xPos > 0 && xPos < area.size() - 1 && area[xPos - 1].blocks.size() == area[xPos].blocks.size() && area[xPos + 1].blocks.size() == area[xPos].blocks.size());
+        (xPos == 0 && area[xPos + 1].blocks.size() <= area[xPos].blocks.size()) ||
+        (xPos == area.size() - 1 && area[xPos - 1].blocks.size() <= area[xPos].blocks.size()) ||
+        (xPos > 0 && xPos < area.size() - 1 && area[xPos - 1].blocks.size() <= area[xPos].blocks.size() && area[xPos + 1].blocks.size() <= area[xPos].blocks.size());
 }
 
 bool isProfitable(vector<column>& area, int i) {
+    // A profit of 0 can free other blocks -> must be considered
     return
         (i == 0 && area[i].remainingValue + area[i + 1].remainingValue >= 0) ||
         (i == area.size() - 1 && area[i].remainingValue + area[i - 1].remainingValue >= 0) ||
         (i > 0 && i < area.size() - 1 && area[i - 1].remainingValue + area[i].remainingValue + area[i + 1].remainingValue >= 0);
 }
 
-// Look at adjacent columns to see if its worth digging them in order to reach the center column's deep block
+// Looks at adjacent columns to see if it's worth digging them in order to reach the center column's deep block
 int findProfitableColumn(vector<column>& area) {
     srand(time(NULL));
     for (int i = 0; i < area.size(); i++) {
@@ -60,74 +48,37 @@ int findProfitableColumn(vector<column>& area) {
     return -1;
 }
 
+// Performs a block extraction on top of a column
 void digColumn(vector<column>& area, int pos) {
-    area[pos].remainingValue -= area[pos].blocks.top().netGain;
-    profit += area[pos].blocks.top().netGain;
-    cout << area[pos].blocks.size() << " " << pos << endl;
+    area[pos].remainingValue -= area[pos].blocks.top();
+    profit += area[pos].blocks.top();
+    result.push_back(make_pair(n - area[pos].blocks.size(), pos));
     area[pos].blocks.pop();
 }
 
+// Checks if all blocks that can be digged return a negative profit
 bool isOnlyNegativeGainAvailable(vector<column>& area) {
     for (int i = 0; i < area.size(); i++) {
-        if (!area[i].blocks.empty() && area[i].blocks.top().netGain >= 0 && canDig2(area, i))
+        if (!area[i].blocks.empty() && area[i].blocks.top() >= 0 && canDig(area, i))
             return false;
     }
     return true;
 }
 
-void extract(vector<column>& area) {
+void extract(vector<column> area) {
     int attempts = 0;
     while (true) {
         int col = findProfitableColumn(area);
-        if (col != -1 && !canDig2(area, col)) {
+        if (!canDig(area, col)) {
             attempts++;
-            if (attempts < 100) continue;
+            if (attempts < 100000) continue; // Treshold after which we juge nothing better can be done
             return;
         }
-        else if (col == -1) return;
 
         attempts = 0;
         for (int i = 0; i < area.size(); i++) {
-            while (!area[i].blocks.empty() && area[i].blocks.top().netGain >= 0 && canDig2(area, i))
+            while (!area[i].blocks.empty() && area[i].blocks.top() >= 0 && canDig(area, i))
                 digColumn(area, i);
-        }
-
-        if (isOnlyNegativeGainAvailable(area)) {
-            int profitableColumn = col;
-            if (profitableColumn == 0) {
-                while ((!area[profitableColumn].blocks.empty() || !area[profitableColumn + 1].blocks.empty()) && isProfitable(area, profitableColumn)) {
-                    if (canDig2(area, profitableColumn)) 
-                        digColumn(area, profitableColumn);
-                    if (canDig2(area, profitableColumn + 1))
-                        digColumn(area, profitableColumn + 1);
-                    if (!(canDig2(area, profitableColumn) || canDig2(area, profitableColumn + 1)))
-                        break;
-                    cout << "im while 1" << endl;
-                }
-            } else if (profitableColumn == area.size() - 1) {
-                while ((!area[profitableColumn].blocks.empty() || !area[profitableColumn - 1].blocks.empty()) && isProfitable(area, profitableColumn)) {
-                    if (canDig2(area, profitableColumn))
-                        digColumn(area, profitableColumn);
-                    if (canDig2(area, profitableColumn - 1))
-                        digColumn(area, profitableColumn - 1);
-                    if (!(canDig2(area, profitableColumn - 1) || canDig2(area, profitableColumn)))
-                        break;
-                    cout << "im while 2" << endl;
-                }
-            } else {
-                while ((!area[profitableColumn - 1].blocks.empty() || !area[profitableColumn].blocks.empty() || !area[profitableColumn + 1].blocks.empty()) && isProfitable(area, profitableColumn)) {
-                    if (canDig2(area, profitableColumn))
-                        digColumn(area, profitableColumn);
-                    if (canDig2(area, profitableColumn - 1))
-                        digColumn(area, profitableColumn - 1);
-                    if (canDig2(area, profitableColumn + 1))
-                        digColumn(area, profitableColumn + 1);
-
-                    if (!(canDig2(area, profitableColumn - 1) || canDig2(area, profitableColumn) || canDig2(area, profitableColumn + 1)))
-                        break;
-                    cout << "im while 3" << endl;
-                }
-            }
         }
     }
 }
@@ -137,51 +88,61 @@ int main(int argc, char* argv[]) {
     // Read program argument
     string file_path;
     string arg(argv[1]);
-    if (arg == "-e" && argc == 3) {
+    if (arg == "-e" && argc == 3)
         file_path = argv[2];
-    }
-    else {
+    else
         cout << "[ERREUR] Rappel: ./tp.sh -e CHEMIN_EXEMPLAIRE" << endl;
-    }
 
     fstream ex_file(file_path);
 
     ex_file >> n;
     ex_file >> m;
 
-    vector<vector<block>> site;
-    vector<column> area;
+    vector<vector<int>> site; //     Only used to fill
+    vector<column> area;      //  <- this variable
 
     int data;
     // Fill with gold values
     for (int i = 0; i < n; i++) {
-        site.push_back(vector<block>());
+        site.push_back(vector<int>());
         for (int j = 0; j < m; j++) {
             ex_file >> data;
-            site[i].push_back({ data, false });
+            site[i].push_back(data);
         }
     }
     // Deduce costs
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < m; j++) {
             ex_file >> data;
-            site[i][j].netGain -= data;
-            //remainingGains += data;
+            site[i][j] -= data;
         }
     }
+    // Create a vector of columns (stacks)
     for (int i = n - 1; i >= 0; i--) {
         for (int j = 0; j < m; j++) {
             if (i == n - 1) {
-                stack<block> newStack;
-                area.push_back({newStack, 0});
+                stack<int> newStack;
+                area.push_back({ newStack, 0 });
             }
             area[j].blocks.push(site[i][j]);
-            area[j].remainingValue += site[i][j].netGain;
+            area[j].remainingValue += site[i][j];
         }
     }
     
-    extract(area);
-    cout << "Total profit : " << profit << endl;
+    const int numReplays = 200; // Arbitrary value to prevent force stop after 3 min
+    int i = 0;
+    while (i < numReplays) {
+        profit = 0;
+        extract(area);
+        if (profit > bestScore) {
+            bestScore = profit;
+            cout << endl;
+            for (auto p : result)
+                cout << p.first << " " << p.second << endl;
+        }
+        result.clear();
+        i++;
+    }
 
     return 0;
 }
